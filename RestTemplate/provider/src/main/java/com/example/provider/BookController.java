4d24e62e562d22490211717242920b029d1cf4e9
JoeeeeeeeyLee <1063319118@qq.com>
2020-09-04 17:33:39 +0800

    RestTemplate

---
+++ RestTemplate/provider/src/main/java/com/example/provider/BookController.java
@@ -0,0 +1,56 @@
+package com.example.provider;
+
+import com.example.Book;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    private final Logger logger=Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        List<ServiceInstance> instances=client.getInstances("hello-service");
+        for (int i=0;i<instances.size();i++){
+            ServiceInstance instance=instances.get(i);
+            logger.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
+        }
+        return "hello";
+    }
+
+    @RequestMapping(value = "/sayhello",method = RequestMethod.GET)
+    public String sayHello(String name){
+        return "hello"+name;
+    }
+
+    @RequestMapping(value = "/getbook1",method = RequestMethod.GET)
+    public Book book1(){
+        return new Book("三国演义",90,"罗贯中","出版社");
+    }
+
+    @RequestMapping(value = "/getbook2",method = RequestMethod.POST)
+    public Book book2(@RequestBody Book book){
+        System.out.println(book.getName());
+        book.setPrice(33);
+        book.setAuthor("吴承恩");
+        book.setPublisher("人民出版社");
+        return book;
+    }
+
+    @RequestMapping(value = "/getbook3/{id}",method = RequestMethod.PUT)
+    public void book3(@RequestBody Book book, @PathVariable int id){
+        logger.info("book:"+book);
+        logger.info("id:"+id);
+    }
+
+    @RequestMapping(value = "/getbook4/{id}",method = RequestMethod.DELETE)
+    public void book4(@PathVariable int id){
+        logger.info("id:"+id);
+    }
+}
---
+++ RestTemplate/provider/src/test/java/com/example/provider/ProviderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.provider;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
