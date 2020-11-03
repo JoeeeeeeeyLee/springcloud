c2b289a464683cdf2bde11c20a46fdb4e90af680
JoeeeeeeeyLee <libaoyu@qianxin.com>
2020-11-03 13:23:28 +0800

    springcloud

---
+++ eurekaserver/src/test/java/org/google/eurekaserver/ServiceproviderApplicationTests.java
@@ -0,0 +1,14 @@
+package org.google.eurekaserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
